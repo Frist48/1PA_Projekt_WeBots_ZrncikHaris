1604061a0a82cc4b2f03b964d7ca05d269c641df
Frist48 <48180718+Frist48@users.noreply.github.com>
2020-12-17 21:23:14 +0100

    Algorythms added (testing)

--- 1PA projekt/controllers/rocket1/rocket1.m
+++ 1PA projekt/controllers/rocket1/rocket1.m
@@ -18,6 +18,8 @@ wb_motor_set_position(front_motor1,inf);
 wb_motor_set_position(rear_motor1,inf);
 wb_motor_set_velocity(front_motor1,-1);
 wb_motor_set_velocity(rear_motor1,-1);
+wb_motor_set_acceleration(front_motor1, 50);
+wb_motor_set_acceleration(rear_motor1, 50);
 
 ds1=wb_robot_get_device('ds1');
 wb_distance_sensor_enable(ds1,TIME_STEP);
@@ -28,6 +30,7 @@ lidar1=wb_robot_get_device('lidar1');
 wb_lidar_enable(lidar1,TIME_STEP);
 wb_lidar_enable_point_cloud(lidar1);
 
+offset = 0;
 % get and enable devices, e.g.:
 %  camera = wb_robot_get_device('camera');
 %  wb_camera_enable(camera, TIME_STEP);
@@ -42,10 +45,62 @@ while wb_robot_step(TIME_STEP) ~= -1
   wall_proximity11=wb_distance_sensor_get_value(ds1);
   wall_proximity12=wb_distance_sensor_get_value(ds12);
   
-  if wall_proximity11<75 | wall_proximity12<75
+  %if wall_proximity11<50 | wall_proximity12<50
+  %  wb_motor_set_velocity(front_motor1,0);
+  %  wb_motor_set_velocity(rear_motor1,0);
+  %end
+  
+  
+  if wall_proximity11 > 128
+    offset = 0
+  elseif wall_proximity11 < 128 & wall_proximity11 > 91
+    offset =  -4
+  elseif wall_proximity11 < 92 & wall_proximity11 > 54
+    offset =  -5
+  elseif wall_proximity11 < 55
+    offset =  -6
+  end
+  
+  if wall_proximity12 > 128
+    offset = 0
+  elseif wall_proximity12 < 128 & wall_proximity12 > 91
+    offset =  4
+  elseif wall_proximity12 < 92 & wall_proximity12 > 54
+    offset =  5
+  elseif wall_proximity12 < 55
+    offset =  6
+  end
+  
+  
+  image = wb_lidar_get_range_image(lidar1)
+  i = 67 + offset;
+  c = 1;
+  buff(c) = image(i-1);
+  c = 2;
+  while i < (90 + offset)
+    buff(c) = image(i);
+    delta_buff(c) = abs(buff(c) - buff(c - 1));
+    c=c+1;
+    i=i+1;
+  end
+  
+  buff
+  %min_value = min(buff)
+  max_delta = max(delta_buff)
+  %ball_pos = find(abs(buff-min_value) < 0.001)
+  ball_pos = find(abs(delta_buff-max_delta) < 0.001)
+  
+  if ball_pos < 12 & wall_proximity12>40
+    wb_motor_set_velocity(front_motor1,-4);
+    wb_motor_set_velocity(rear_motor1,-4);
+  elseif ball_pos > 12 & wall_proximity11>40
+    wb_motor_set_velocity(front_motor1,4);
+    wb_motor_set_velocity(rear_motor1,4);
+  elseif ball_pos == 0 | 12 
     wb_motor_set_velocity(front_motor1,0);
     wb_motor_set_velocity(rear_motor1,0);
   end
+  
   % read the sensors, e.g.:
   %  rgb = wb_camera_get_image(camera);
 
--- 1PA projekt/controllers/rocket2/rocket2.m
+++ 1PA projekt/controllers/rocket2/rocket2.m
@@ -18,6 +18,8 @@ wb_motor_set_position(front_motor2,inf);
 wb_motor_set_position(rear_motor2,inf);
 wb_motor_set_velocity(front_motor2,-1);
 wb_motor_set_velocity(rear_motor2,-1);
+wb_motor_set_acceleration(front_motor2, 50);
+wb_motor_set_acceleration(rear_motor2, 50);
 
 ds21=wb_robot_get_device('ds21');
 wb_distance_sensor_enable(ds21,TIME_STEP);
@@ -27,6 +29,7 @@ wb_distance_sensor_enable(ds22,TIME_STEP);
 lidar2=wb_robot_get_device('lidar2');
 wb_lidar_enable(lidar2,TIME_STEP);
 wb_lidar_enable_point_cloud(lidar2);
+offset = 0;
 
 
 
@@ -45,7 +48,59 @@ while wb_robot_step(TIME_STEP) ~= -1
   wall_proximity21=wb_distance_sensor_get_value(ds21);
   wall_proximity22=wb_distance_sensor_get_value(ds22);
   
-  if wall_proximity21<75 | wall_proximity22<75
+  %if wall_proximity21<75 | wall_proximity22<75
+  %  wb_motor_set_velocity(front_motor2,0);
+  %  wb_motor_set_velocity(rear_motor2,0);
+  %end
+  
+    
+  if wall_proximity22 > 127
+    offset = 0
+  elseif wall_proximity22 < 128 & wall_proximity22 > 91
+    offset =  -4
+  elseif wall_proximity22 < 92 & wall_proximity22 > 54
+    offset =  -5
+  elseif wall_proximity22 < 55
+    offset =  -6
+  end
+  
+  if wall_proximity21 > 128
+    offset = 0
+  elseif wall_proximity21 < 128 & wall_proximity21 > 91
+    offset =  4
+  elseif wall_proximity21 < 92 & wall_proximity21 > 54
+    offset =  5
+  elseif wall_proximity21 < 55
+    offset =  6
+  end
+  
+
+  image = wb_lidar_get_range_image(lidar2)
+  i = 67 + offset;
+  c = 1;
+  buff(c) = image(i-1);
+  c = 2;
+  while i < (90 + offset)
+    buff(c) = image(i);
+    delta_buff(c) = abs(buff(c) - buff(c - 1));
+    c=c+1;
+    i=i+1;
+  end
+  
+  buff
+  %min_value = min(buff)
+  max_delta = max(delta_buff)
+  %ball_pos = find(abs(buff-min_value) < 0.001)
+  ball_pos = find(abs(delta_buff-max_delta) < 0.001)
+  
+  
+  if ball_pos < 12 & wall_proximity21>40
+    wb_motor_set_velocity(front_motor2,-4);
+    wb_motor_set_velocity(rear_motor2,-4);
+  elseif ball_pos > 12 & wall_proximity22>40
+    wb_motor_set_velocity(front_motor2,4);
+    wb_motor_set_velocity(rear_motor2,4);
+  elseif ball_pos == 0 | 12
     wb_motor_set_velocity(front_motor2,0);
     wb_motor_set_velocity(rear_motor2,0);
   end
